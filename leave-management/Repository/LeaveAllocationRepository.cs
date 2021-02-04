68a7f3c34f8fecb48d953d140d5e76dfe9ba3183
Erdem <erdem_oztan@msn.com>
2021-02-04 12:02:03 +0100

    Add project files.

---
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,59 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            return _db.LeaveAllocations.ToList();
+        }
+
+        public LeaveAllocation FindById(string id)
+        {
+            int idInt;
+
+            if (!int.TryParse(id, out idInt))
+            {
+                return null;
+            }
+
+            return _db.LeaveAllocations.FirstOrDefault(q => q.Id == idInt);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() > 0;
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Update(entity);
+            return Save();
+        }
+    }
+}
---
+++ leave-management/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,64 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public bool Create(LeaveType entity)
+        {
+            _db.LeaveTypes.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+           return _db.LeaveTypes.ToList();
+        }
+
+        public LeaveType FindById(string id)
+        {
+            int idInt;
+
+            if(!int.TryParse(id, out idInt))
+            {
+                return null;
+            }
+
+            return _db.LeaveTypes.Where(q => q.Id == idInt).FirstOrDefault();
+        }
+
+        public ICollection<Employee> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges()>0;
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            return Save();
+        }
+    }
+}
