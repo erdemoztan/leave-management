a2834efa427d4b824e9e5e7e9654b60b19f0ce20
Erdem <erdem_oztan@msn.com>
2021-02-08 15:38:36 +0100

    User management

---
+++ leave-management/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedData.SeedRole(roleManager);
+            SeedData.SeedUser(userManager);
+        }
+
+        private static void SeedUser(UserManager<IdentityUser> userManager)
+        {
+            if(userManager.FindByNameAsync("admin@localhost.com").Result == null)
+            {
+                var user = new IdentityUser { UserName = "admin@localhost.com", Email = "admin@localhost.com" };
+                var result = userManager.CreateAsync(user, "P@ssword123").Result;
+
+                if(result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+
+            }
+        }
+
+        private static void SeedRole(RoleManager<IdentityRole> roleManager)
+        {
+            if(!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                roleManager.CreateAsync(new IdentityRole { Name = "Admin" }).Wait();
+            }
+
+            if (!roleManager.RoleExistsAsync("Employee").Result)
+            {
+                roleManager.CreateAsync(new IdentityRole { Name = "Employee" }).Wait();
+            }
+        }
+    }
+}
